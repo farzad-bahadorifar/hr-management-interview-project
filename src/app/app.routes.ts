19aa145107aac554358a64a20840d244ae281040
Farzad Bahadorifar <34229837+f6rz9d@users.noreply.github.com>
2024-10-27 00:21:03 +0330

    feat: add app.route for routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeDetailsComponent } from './features/employee-details/employee-details.component';
+import { UnitListComponent } from './features/units/unit-list/unit-list.component';
+import { EmployeeListComponent } from './features/employees/employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'units', pathMatch: 'full' },
+  { path: 'units', component: UnitListComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/:id', component: EmployeeDetailsComponent },
+];
